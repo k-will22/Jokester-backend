896c72b3c47e4fb349f8b54d227a8969cb32fc0e
anberjamz <anberjam@yahoo.com>
2021-01-22 15:39:59 -0500

    created all migrations models and worked on jokes controller

---
+++ app/controllers/jokes_controller.rb
@@ -0,0 +1,19 @@
+class JokesController < ApplicationController
+
+    def index
+      jokes = Joke.all
+      render json: jokes
+    end
+
+    def create 
+        joke = Joke.create({joke: params[:joke], category: params[:category]})
+        render json: joke
+    end
+
+    private
+
+    def joke_params
+        params.require(:joke).permit(:joke, :category)
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_023236) do
+ActiveRecord::Schema.define(version: 2021_01_22_184959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "joke_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["joke_id"], name: "index_favorites_on_joke_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "jokes", force: :cascade do |t|
     t.string "joke"
@@ -22,4 +31,12 @@ ActiveRecord::Schema.define(version: 2021_01_22_023236) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "favorites", "jokes"
+  add_foreign_key "favorites", "users"
 end
