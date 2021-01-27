25084ab3211b59e42e7620dbbb55ce24d8d21c34
k-will22 <thestreetkw@hotmail.com>
2021-01-27 18:24:54 -0500

    deleted joke

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@ Joke.destroy_all
 Joke.create(category: "Misc", joke: "What's the difference between an in-law and an outlaw? An outlaw is wanted.")
 Joke.create(category: "Pun", joke: "Where do sick cruise ships go to get healthy? The dock!")
 Joke.create(category: "Misc", joke: "My mother said, 'You won't amount to anything because you always procrastinate.' I said, 'Oh yeah... Just you wait.'")
-Joke.create(category: "Dark", joke: "9/11 jokes are not funny. The other 2 though, are hilarious!")
 Joke.create(category: "Programming", joke: "Programming is 10% science, 20% ingenuity, and 70% getting the ingenuity to work with the science." )
 Joke.create(category: "Spooky", joke: "What's it like to be kissed by a vampire? It's a pain in the neck." )
 Joke.create(category: "Dark", joke: "My little daughter came to me all excited, saying 'Daddy! Daddy! Guess how old I'll be in June!' 'Oh I don't know princess, why don't you tell me?' I said. She gave me a huge smile and held up four fingers. It's now three hours later, police have joined in and she still won't say where she got them." )
